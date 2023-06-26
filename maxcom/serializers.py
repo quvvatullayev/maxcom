0e2b995a478d817b792014a14d6195491bfe460f
ogabek <quvvatullayev@gmail.com>
2023-06-26 10:27:30 +0500

    create serializers modle

---
+++ maxcom/serializers.py
@@ -0,0 +1,101 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import (
+    Category,
+    SubCategory,
+    Product,
+    ProductImage,
+    Like,
+    Card,
+    Certificate,
+    Contaket,
+    About,
+    CompanyQuestion,
+)
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = '__all__'
+
+class SubCategorySerializer(serializers.ModelSerializer):
+    category = CategorySerializer()
+    class Meta:
+        model = SubCategory
+        fields = ['id', 'category', 'name', 'description']
+    
+class CreateSubCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = SubCategory
+        fields = "__all__"
+
+class ProductSerializer(serializers.ModelSerializer):
+    subcategory = SubCategorySerializer()
+    class Meta:
+        model = Product
+        fields = ['id', 'subcategory', 'name', 'brand', 'description', 'price', 'discount', 'image', 'short_description', 'live', 'like', 'status']
+
+class CreateProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = "__all__"
+
+class ProductImageSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+    class Meta:
+        model = ProductImage
+        fields = ['id', 'product', 'image']
+
+class CreateProductImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProductImage
+        fields = "__all__"
+
+class LikeSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+    user = serializers.StringRelatedField()
+    class Meta:
+        model = Like
+        fields = ['id', 'product', 'user']
+
+class CreateLikeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Like
+        fields = "__all__"
+
+class CardSerializer(serializers.ModelSerializer):
+    product = ProductSerializer()
+    user = serializers.StringRelatedField()
+    class Meta:
+        model = Card
+        fields = ['id', 'product', 'cout', 'user']
+
+class CreateCardSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Card
+        fields = "__all__"
+
+class CertificateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Certificate
+        fields = "__all__"
+
+class ContaketSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Contaket
+        fields = "__all__"
+
+class AboutSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = About
+        fields = "__all__"
+
+class CompanyQuestionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CompanyQuestion
+        fields = "__all__"
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password', 'email']       
