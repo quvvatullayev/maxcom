b3b37600aa205f4b2f4721279bd4f7186f93ad79
ogabek <quvvatullayev@gmail.com>
2023-07-07 20:45:11 +0500

    create sms email send messege

--- maxcom/views/order.py
+++ maxcom/views/order.py
@@ -5,6 +5,7 @@ from rest_framework import status
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.authtoken.models import Token
 from rest_framework.authentication import TokenAuthentication
+import smtplib
 from ..models import (
     Card,
     Product,
@@ -45,6 +46,30 @@ class OrderCreateView(APIView):
         if serializer.is_valid():
             serializer.save()
             card.delete()
+
+            sender = "quvvatullayev@gmail.com"
+            receiver = user.email
+
+            message = f"""\
+            Subject: Hi Mailtrap
+            To: {receiver}
+            From: {sender}
+
+            user = {user.name}\n
+            product = {data['product']["name"]}\n
+            brand = {data['product']['brand']}\n
+            description = {data['product']['description']}\n
+            price = {data['product']['price']}
+            address = {data['address']}\n
+            phone = {data['phone']}\n
+            date = {data['date']}\n
+            total = {data['total']}\n
+            count = {data['cout']*data['product']['price']}"""
+
+            with smtplib.SMTP("sandbox.smtp.mailtrap.io", 2525) as server:
+                auth = server.login("e83e926941c981", "268821db4f6c1f")
+                send = server.sendmail(sender, receiver, message)
+            
             return Response(serializer.data, status=status.HTTP_201_CREATED,)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST,)
     
